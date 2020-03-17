df5f236914c5f1b599066b57610b18291221af8c
tdakkota <tanc13@yandex.ru>
2020-03-18 00:43:42 +0300

    fix: update cli tool

--- cmd/cli/app.go
+++ cmd/cli/app.go
@@ -1,31 +1,107 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/tdakkota/gnhentai"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
 )
 
-func runner(c *cli.Context) error {
+type App struct {
+	client *gnhentai.Client
+}
+
+func NewApp() *App {
+	return &App{
+		client: gnhentai.NewClient(),
+	}
+}
+
+func prettyPrint(d gnhentai.Doujinshi) error {
+	data, err := json.Marshal(d)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(data))
 	return nil
 }
 
-func commands() []*cli.Command {
+func (app *App) random(c *cli.Context) error {
+	doujinshi, err := app.client.Random()
+	if err != nil {
+		return err
+	}
+
+	err = prettyPrint(doujinshi)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *App) get(c *cli.Context) error {
+	doujinshi, err := app.client.ByID(c.Int("id"))
+	if err != nil {
+		return err
+	}
+
+	err = prettyPrint(doujinshi)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *App) commands() []*cli.Command {
 	return []*cli.Command{
-		{Name: "random"},
-		{Name: "get"},
+		{
+			Name:   "random",
+			Action: app.random,
+		},
+		{
+			Name: "get",
+			Flags: []cli.Flag{
+				&cli.IntFlag{
+					Name:  "id",
+					Usage: "id of doujinshi",
+				},
+			},
+			Action: app.get,
+		},
 	}
 }
 
-func main() {
-	app := &cli.App{
-		Name:   "gnhentai-cli",
-		Usage:  "cli tool to search Doujinshi",
-		Action: runner,
+func (app *App) flags() []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:    "proxy",
+			Usage:   "proxy for http client (in format socks5://localhost:9051)",
+			EnvVars: []string{"PROXY"},
+		},
+	}
+}
+
+func (app *App) cli() *cli.App {
+	return &cli.App{
+		Name:     "gnhentai-cli",
+		Usage:    "cli tool to search Doujinshi",
+		Commands: app.commands(),
+		Flags:    app.flags(),
 	}
 
-	err := app.Run(os.Args)
-	if err != nil {
+}
+
+func (app *App) Run(args []string) error {
+	return app.cli().Run(args)
+}
+
+func main() {
+	if err := NewApp().Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
